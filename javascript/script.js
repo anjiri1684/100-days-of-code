e76763d7c2fff94a4b091527a2f54d7ba0ab1ccb
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-15 11:31:30 +0300

    started learning javascript from scratch along side building projects

---
+++ javascript/script.js
@@ -0,0 +1,41 @@
+// this is a single line commnet
+/*this
+is
+a
+Multiline
+comment*/
+//console.log(typeof "Vincent");
+//console.log(typeof 42);
+//console.log(typeof true);
+//let myVariable;
+//console.log(typeof myVariable);
+//myVariable = "Vincent";
+//console.log(typeof myVariable);
+//myVariable = 42;
+//console.log(typeof myVariable);
+//let myNumber = 42;
+//console.log(myNumber + 10);
+//alert("Hello World");
+
+//strings
+//const myVariable = "Mathematics";
+
+//strings methods
+/* console.log(myVariable.length);
+console.log(myVariable.charAt(6));
+console.log(myVariable.indexOf("mat"));
+console.log(myVariable.lastIndexOf("a"));
+console.log(myVariable.slice(5, 8));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("ma"));
+console.log(myVariable.split("")); */
+
+//Numbers
+//const myNumber = 42;
+//console.log(myNumber);
+
+//const myFloat = 42.01;
+//const myString = "42";
+//console.log(typeof myFloat);
+//console.log(+myString + 24);
