719e98b2d2111f55d8365ce6eda5ca62896e05dd
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-16 16:17:57 +0300

    "Added react-icons dependency and implemented a todo list feature in Content component"

--- reactjs/01tut/src/components/content/Content.jsx
+++ reactjs/01tut/src/components/content/Content.jsx
@@ -1,25 +1,65 @@
+import { useState } from "react";
 import classes from "./content.module.css";
+import { FaTrashAlt } from "react-icons/fa";
 const Content = () => {
-  const handleNameChange = () => {
-    const names = [
-      "Ava",
-      "Liam",
-      "Sofia",
-      "Noah",
-      "Isabella",
-      "Mason",
-      "Mia",
-      "Elijah",
-      "Emma",
-      "Olive",
-    ];
-    const randomPos = Math.floor(Math.random() * names.length);
-    return names[randomPos];
-  };
+  const [items, setItems] = useState([
+    { id: 1, checked: true, item: "Milk" },
+    { id: 2, checked: false, item: "Eggs" },
+    { id: 3, checked: true, item: "Bread" },
+    { id: 4, checked: false, item: "Butter" },
+    { id: 5, checked: true, item: "Cheese" },
+    { id: 6, checked: false, item: "Tomatoes" },
+    { id: 7, checked: true, item: "Apples" },
+  ]);
+
+  function handleCheck(id) {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+  }
+
+  function handleDelete(id) {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+  }
+
   return (
     <div className={classes.content}>
       <main>
-        <p>Hello {handleNameChange()}</p>
+        {items.length ? (
+          <ul>
+            {items.map((item) => {
+              return (
+                <li key={item.id} className={classes.item}>
+                  <input
+                    type="checkbox"
+                    checked={item.checked}
+                    onChange={() => handleCheck(item.id)}
+                  />
+                  <label
+                    style={
+                      item.checked ? { textDecoration: "line-through" } : null
+                    }
+                    onDoubleClick={() => handleCheck(item.id)}
+                  >
+                    {item.item}
+                  </label>
+
+                  <FaTrashAlt
+                    onClick={() => handleDelete(item.id)}
+                    role="button"
+                    tabIndex="0"
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <p>No items Found</p>
+        )}
       </main>
     </div>
   );
