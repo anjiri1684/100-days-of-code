af48509131c6d4ae3460aab93fbbb8ee6a9eb0c6
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-16 13:42:34 +0300

    started learning reactjs and building projects alongside learning

---
+++ reactjs/01tut/src/App.jsx
@@ -0,0 +1,11 @@
+import classes from "./app.module.css";
+
+function App() {
+  return (
+    <div className={classes.container}>
+      <h1>My First React App</h1>
+    </div>
+  );
+}
+
+export default App;
