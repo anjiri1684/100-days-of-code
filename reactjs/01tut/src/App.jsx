51e9131e141fefc816b4e9e7b4283d6815f7ef6d
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-16 13:57:21 +0300

    "Updated App.jsx to display a random name from a list, and reformatted code in main.jsx"

--- reactjs/01tut/src/App.jsx
+++ reactjs/01tut/src/App.jsx
@@ -1,9 +1,22 @@
 import classes from "./app.module.css";
 
 function App() {
+  const names = [
+    "Ava",
+    "Liam",
+    "Sofia",
+    "Noah",
+    "Isabella",
+    "Mason",
+    "Mia",
+    "Elijah",
+    "Emma",
+    "Oliver",
+  ];
+  const randNum = Math.floor(Math.random() * 4);
   return (
     <div className={classes.container}>
-      <h1>My First React App</h1>
+      <h1>My name is {names[randNum]}</h1>
     </div>
   );
 }
