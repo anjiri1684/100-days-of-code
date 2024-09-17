4501b19d469cffd4554ceb7ebe3606ef09432632
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-17 13:52:46 +0300

    started lerning python

---
+++ python/day1/welcome.py
@@ -0,0 +1,13 @@
+line01 = '********************'
+line02 = '*                  *'
+line03 = '*     WELCOME!     *'
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+
+
