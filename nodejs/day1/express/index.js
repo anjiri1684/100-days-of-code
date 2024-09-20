81a5345b98f00da90a318e8d0a777cb0a29ec0ef
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-20 15:06:03 +0300

    "Added new route for /employees and modified 404 handling to respond with HTML, JSON, or plain text based on request type"

--- nodejs/day1/express/index.js
+++ nodejs/day1/express/index.js
@@ -36,9 +36,17 @@ app.use("/", require("./routes/roots"));
 app.use("/subdir", express.static(path.join(__dirname, "public")));
 
 app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "4040 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 app.use(errorHandler);
---
+++ nodejs/day1/express/routes/api/employees.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+const data = {};
+data.employess = require("../../data/employees.json");
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(data.employess);
+  })
+  .post((req, res) => {
+    res.json({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      message: "Employee added successfully",
+    });
+  })
+  .put((req, res) => {
+    res.json({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+  })
+  .delete((req, res) => {
+    res.json({
+      id: req.body.firstname,
+      message: "Employee deleted successfully",
+    });
+  });
+
+router.route("/:id").get((req, res) => {
+  res.json({
+    id: req.params.id,
+  });
+});
+
+module.exports = router;
