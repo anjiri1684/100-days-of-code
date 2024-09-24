909b10e74eb3d97efd1a35e94a1599e109ae9d6e
vincent anjiri <vincentanjiri12@gmail.com>
2024-09-24 13:03:48 +0300

    "Remove Button and Counter components, modify MyComponent, and delete ProfilePicture component"

--- reactjs/website/src/MyComponent.jsx
+++ reactjs/website/src/MyComponent.jsx
@@ -1,32 +1,15 @@
 import { useState } from "react";
 
 function MyComponent() {
-  const [name, setName] = useState("Guest");
-  const [age, setAge] = useState(0);
-  const [isEmployed, setIsEmployed] = useState(false);
+  const [name, setName] = useState("");
 
-  const updateName = () => {
-    setName("Vincent");
-  };
-
-  const updateAge = () => {
-    setAge(age + 1);
-  };
-
-  const employmentStatus = () => {
-    setIsEmployed(!isEmployed);
-  };
-
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
   return (
     <div>
+      <input type="text" value={name} onChange={handleNameChange} />
       <p>Name: {name}</p>
-      <button onClick={updateName}>Set Name</button>
-
-      <p>Age: {age}</p>
-      <button onClick={updateAge}>Increment Age</button>
-
-      <p>Is Employed: {isEmployed ? "Yes" : "No"}</p>
-      <button onClick={employmentStatus}>Set Name</button>
     </div>
   );
 }
